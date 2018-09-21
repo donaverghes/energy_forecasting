0b551e4de21669902f9dd4c0df3ecbafc1e9051e
Sarah Ewing <onyxsarah@gmail.com>
2018-09-21 09:32:08 -0700

    re org folder

--- Sarah/Energy_ts/global.r
+++ Sarah/Energy_ts/global.r
@@ -1,28 +1,84 @@
+rm(list = ls())
 
-setwd("/Users/sarahewing/Documents/OnBoarding/energy_forecasting/Energy_ts")
+getwd()
 ls()
 list.files(getwd())
 
+library(googleVis)
+library(quantmod)
 library(shiny)
 library(ggplot2)
+library(forecast)
+library(TTR)
 library(zoo)
+library(shinydashboard)
+library(DT)
+library(tm)
+library(ggmap)
 
-raw = read.csv("/Users/sarahewing/Documents/OnBoarding/energy_forecasting/Energy_ts/data/sarah_data.csv", header = F)
+library(RCurl)
 
-## the names of the variables
-colnames(raw) = c("prediction_date", "wind_speed_100m", 
-                  "wind_direction_100m", 
-                  "temperature", 
-                  "air_density",
-                  "pressure", "precipitation", "wind_gust", "radiation", 
-                  "wind_speed", "wind_direction", "price")
-## format the date
-raw$prediction_dttm = as.POSIXct(raw$prediction_date, tz = "UTC", 
-                                 origin = "1970-01-01", format = "%Y-%m-%d %H:%M:%S")
-raw$prediction_date = as.Date(substr(raw$prediction_date,1,11))
-raw = raw[order(raw$prediction_date),]
+## RNN
+hRNN <- getURL('https://raw.githubusercontent.com/sarah-ewing/energy_forecasting/master/discovery/rnnn.csv')
+RNN <- read.csv(text = hRNN, header=F)
+colnames(RNN) = c("Dttm", "price","P_price")
+RNN$Dttm = as.POSIXct(RNN$Dttm,
+                        tz = "UTC",
+                        origin = "1970-01-01",
+                        format="%Y-%m-%d %H:%M:%S")
+RNN_P_price = c(rep(0,5554),RNN$P_price)
 
-source("ui.r")
-source("server.r")
-# Start Shiny app ----
-shinyApp(ui = ui, server = server)
+## DNN
+hDNN <- getURL('https://raw.githubusercontent.com/sarah-ewing/energy_forecasting/master/discovery/dnn.csv')
+DNN <- read.csv(text = hDNN, header=F)
+colnames(DNN) = c("Dttm", "price","P_price")
+DNN$Dttm = as.POSIXct(DNN$Dttm,
+                      tz = "UTC",
+                      origin = "1970-01-01",
+                      format="%Y-%m-%d %H:%M:%S")
+DNN_P_price = c(rep(0,5554),DNN$P_price)
+
+
+## CNN
+hCNN <- getURL('https://raw.githubusercontent.com/sarah-ewing/energy_forecasting/master/discovery/cnn.csv')
+CNN <- read.csv(text = hCNN, header=F)
+colnames(CNN) = c("Dttm", "price","P_price")
+CNN$Dttm = as.POSIXct(CNN$Dttm,
+                      tz = "UTC",
+                      origin = "1970-01-01",
+                      format="%Y-%m-%d %H:%M:%S")
+CNN_P_price = c(rep(0,5554),CNN$P_price)
+
+
+### Autoregressive Informed Moving Average
+ARIMA = read.csv('ARIMA.csv')
+colnames(ARIMA) = c("ID", "Dttm", "price","P_price")
+ARIMA$Dttm = as.POSIXct(ARIMA$Dttm,
+                        tz = "UTC",
+                        origin = "1970-01-01",
+                        format="%Y-%m-%d %H:%M:%S")
+ARIMA$Date = as.Date(ARIMA$Dttm)
+ARIMA = ARIMA[order(ARIMA$Dttm),]
+rownames(ARIMA) = NULL
+
+raw = as.data.frame(cbind(ARIMA[,2:5],DNN_P_price, CNN_P_price, RNN_P_price))
+colnames(raw) = c("predicted_dttm","price", "p_price",
+                  "predicted_date", "DNN", "CNN", "RNN")
+raw$day_week = weekdays(as.Date(raw$predicted_date))
+rm(RNN, ARIMA, hRNN, RNN_P_price)
+
+t(aggregate(raw$price, by=list(raw$day_week), FUN=mean, na.rm=TRUE))
+
+ID = 1:18
+lon = c(-8,-6,-4,-2,-8,-6,-4,-2,-8,-6,-4,-2, 0,-8,-6,-4,-2, 0)
+lat = c(37, 37, 37, 37, 39, 39, 39, 39, 41, 41, 41, 41, 41, 43, 43, 43, 43, 43)
+df = as.data.frame(cbind(ID, lat, lon))
+rm(ID, lon, lat)
+
+#library(googleCloudStorageR)
+#gcs_auth(new_user = TRUE)
+
+## set bucket via environment
+#Sys.setenv("GCS_DEFAULT_BUCKET" = "sarah-bucket")
+#gcs_get_global_bucket()
+#gcs_auth()
--- Sarah/Energy_ts/ui.r
+++ Sarah/Energy_ts/ui.r
@@ -8,33 +8,87 @@
 #           Shiny app
 #
 ##########################################
-
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Energy Forecast"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-       sliderInput(inputId = "prediction_date_range",
+shinyUI(
+  dashboardPage (
+    ###########################################
+    dashboardHeader(title = 'Energy Price Forecast'),
+    dashboardSidebar(
+      # sidebarUserPanel('ATOS_LOGO',
+      #                  image = 'https://github.com/sarah-ewing/energy_forecasting/blob/master/Sarah/Energy_ts/data/ATOS_LOGO.png'),
+      # 
+      #########################################
+      sidebarMenu(
+        menuItem('Data', tabName = 'dt', icon = icon('database')),
+        
+        
+        
+        #########################################
+        ## MAP
+        menuItem('Map', tabName = 'tm', icon = icon('map')),
+        ## MAP
+        
+        #########################################
+        menuItem('TimeSeries', tabName = 'ts', icon = icon('line-chart'))
+      ),###sidebarMenu
+      
+      ####### date range for time series
+      sliderInput(inputId = "prediction_date_range",
                   label = "Date:",
-                  min = as.Date('2015-01-01', origin = "1970-01-01"),
+                  min = as.Date('2015-09-01', origin = "1970-01-01"),
                   max = as.Date('2015-10-20', origin = "1970-01-01"),
-                  value = c(as.Date('2015-01-01', origin = "1970-01-01"),
+                  value = c(as.Date('2015-09-20', origin = "1970-01-01"),
                             as.Date('2015-10-20', origin = "1970-01-01")),
-                  step = 5)
-    ),
+                  step = 5),
+      
+      # the chunk below makes a group of checkboxes
+      checkboxGroupInput(inputId = "ModelTypes", 
+                         label = h3("Models"), 
+                         choices = list("ARIMA" = 1, "DNN" = 3, "CNN" = 4, "RNN" = 5),
+                         selected = NULL),
+      
+      hr(),
+      fluidRow(column(3, verbatimTextOutput("value")))
     
-    # Main panel for displaying outputs ----
-    mainPanel(
       
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
-  )
-)
+      ), ###dashboardSidebar
+    #########################################
+    dashboardBody(tabItems(
+      tabItem(tabName = 'dt',
+              tabBox(
+                title = tagList(shiny::icon("line-chart"), "Time Series"),
+                width = 9,
+                
+                tabPanel('Price',
+                         height = "700px",
+                         width = "900px",
+                         plotOutput('price_ts', 
+                                    height = "700px",
+                                    width = "1000px"))
+              
+                )),## tabItem(tabName = 'dt'
+      #########################################
+      tabItem(tabName = 'tm',
+              tabBox(
+                title = tagList(shiny::icon("map"), "Map"),
+                
+                tabPanel('Weather Stations in Spain & Portugal',
+                         plotOutput('MAP', 
+                                    height = "700px",
+                                    width = "700px"))
+              )), ## tabItem(tabName = 'tm'
+      #########################################
+      tabItem(tabName = 'ts',
+              tabBox(
+                title = tagList(shiny::icon("line-chart"), "Time Series"),
+                height = "700px",
+                width = "1500px",
+                tabPanel('Forecast Price',
+                         height = "700px",
+                         width = "1100px",
+                         plotOutput('fore', 
+                                    height = "700px",
+                                    width = "1100px"))
+              )) ## tabItem(tabName = 'ts')
+      #########################################
+    ))
+  )) ## shinyUI & dashboardPage
